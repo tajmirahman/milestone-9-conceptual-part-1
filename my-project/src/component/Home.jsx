b009b3319ddc12ffabc6e07a021fabe8df582e22
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-26 20:55:21 +0600

    google sign in done with firebase

--- my-project/src/component/Home.jsx
+++ my-project/src/component/Home.jsx
@@ -1,15 +1,25 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import auth from '../firebase.init';
 
 const Home = () => {
 
     const handleGoogleLogin=()=>{
-        
+        const googleProvider= new GoogleAuthProvider();
+
+        signInWithPopup(auth, googleProvider)
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err=>{
+            console.log(err);
+        })
     }
 
     return (
         <div>
             <h1 className='text-3xl text-center'>I am Home page</h1>
-            <button onClick={handleGoogleLogin}>Google Login</button>
+            <button onClick={handleGoogleLogin} className='btn btn-warning text-center'>Google Login</button>
         </div>
     );
 };
